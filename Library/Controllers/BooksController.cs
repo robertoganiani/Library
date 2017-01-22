9665d5542c8aaf395d64e147109a9e8c78af279d
robertoganiani <oganiani90@gmail.com>
2017-01-22 11:55:55 +0400

    initial commit

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Library.Models;
+using Library.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books/Random
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = "The Hunger Games" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomBookViewModel
+            {
+                Book = book,
+                Customers = customers
+            };
+
+            return View(viewModel); 
+        }
+    }
+}
---
+++ Library/ViewModels/RandomBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.ViewModels
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
