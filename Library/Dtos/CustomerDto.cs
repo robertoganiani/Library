2043d5ce085530da39afdd835a33eb51720e46a1
robertoganiani <oganiani90@gmail.com>
2017-01-31 22:55:59 +0400

    added webapi for customers

---
+++ Library/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using Library.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
